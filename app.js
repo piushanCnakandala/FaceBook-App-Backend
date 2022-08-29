a7109a190b0451eb7914c086f20a74c3894bedb1
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-08-29 20:06:48 +0530

    create mongoDb connection

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const mongoose =require('mongoose')
 const app = express()
 const port = 4000
 
+const  url= 'mongodb://localhost/express'
+mongoose.connect(url,{useNewUrlParser:true})
+const con =mongoose.connection
+
+con.on("open",()=>{
+    console.log('mongodb connected')
+})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
