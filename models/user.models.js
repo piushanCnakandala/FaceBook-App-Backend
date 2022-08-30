a017338c53cca9873adb9f7a3438000916a6446a
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-08-30 23:12:03 +0530

    create userSchema

---
+++ models/user.models.js
@@ -0,0 +1,37 @@
+const mongooose  =require('mongoose')
+
+const userSchema =new mongooose.Schema({
+    firstName:{
+        type :String,
+        required:true
+    },
+    surName:{
+        type :String,
+        required:true
+    },
+    gender:{
+        type :String,
+        required:true
+    },
+    dateOfBirth:{
+        type :Date,
+        required:true
+    },
+    password:{
+        type :String,
+        required:true,
+        trim:true
+    },
+    phoneNumber:{
+        type :String,
+        required:true
+    },
+    email:{
+        type :String,
+        required:true,
+        unique:true,
+        trim:true
+    }
+})
+
+module.exports=mongooose.module('User',userSchema)
