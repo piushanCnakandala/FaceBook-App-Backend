f8b5108fb187a639e11032123c4cb86455723e29
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-08-31 00:20:12 +0530

    get all user and save user done

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,38 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 
-const User =require('../models/user.models')
+const User = require('../models/user.models')
 app.use(express.json())
 
-module.exports =router
+
+router.get('/', async (req, res) => {
+    try {
+        const user = await User.find();
+        res.json(user)
+    } catch (err) {
+        res.send("Err : " + err)
+    }
+})
+
+router.post('/',async(req,res)=>{
+   const user =await new User({
+       firstName: req.body.firstName,
+       sureName: req.body.sureName,
+       gender: req.body.gender,
+       dateOfBirth: req.body.dateOfBirth,
+       password: req.body.password,
+       phoneNumber: req.body.phoneNumber,
+       email: req.body.email,
+   })
+
+    try {
+        const user = await user.save();
+        res.json(user)
+    } catch (err) {
+        res.send("Err : " + err)
+    }
+})
+
+
+
+module.exports = router
